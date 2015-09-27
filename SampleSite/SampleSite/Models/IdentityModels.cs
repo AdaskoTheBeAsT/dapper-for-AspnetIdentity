b7a3d6309b241de07c55686f194b94cc858d30f8
Adam Pluciński <adaskothebeast@gmail.com>
2015-09-27 20:12:57 +0200

    Updated sample solution

--- SampleSite/SampleSite/Models/IdentityModels.cs
+++ SampleSite/SampleSite/Models/IdentityModels.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace IdentitySample.Models
 {
-    [Lu.Dapper.Extensions.Utils.Table("IdentityUsers")]
+    [Lu.Dapper.Extensions.Table("IdentityUsers")]
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
@@ -19,7 +19,7 @@ namespace IdentitySample.Models
             return userIdentity;
         }
     }
-    [Lu.Dapper.Extensions.Utils.Table("IdentityRoles")]
+    [Lu.Dapper.Extensions.Table("IdentityRoles")]
     public class ApplicationRole : IdentityRole
     {
     }
